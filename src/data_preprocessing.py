da0b93620a58313e1bbd7e362a17c3be0ef9ecb2
Adityakante <kanteaditya13@gmail.com>
2026-02-11 12:09:09 +0530

    Done code

--- src/data_preprocessing.py
+++
@@ -1,8 +0,0 @@
-from config import Config
-# data preprocessing
-
-def data_preprocessing(df):
-    
-    # segregate numerical and categorical columns
-    numerical_cols = df.select_dtypes(include=['int64', 'float64']).columns
-    categorical_cols = df.select_dtypes(include=['object']).columns
